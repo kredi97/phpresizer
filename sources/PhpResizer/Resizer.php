7e4b464e0823022739eccea77c38461fac939ec8
andrew.stephanoff <andrew.stephanoff@gmail.com>
2010-11-13 23:46:26 +0000

    extract exception to separate file

---
+++ sources/PhpResizer/Resizer.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * PHPResizer
+ *
+ *
+ * @package    PHPResizer
+ * @copyright  http://phpresizer.org/
+ * @author 	   niko
+ * @version    1.0 beta
+ */
+/**
+ * @author niko
+ *
+ */
+
+/**
+ *
+ * @author niko
+ */
+class PhpResizer_Resizer {
+
+    const ENGINE_GD2='gd2';
+    const ENGINE_IMAGEMAGICK='imagemagic';
+    const ENGINE_GRAPHIKSMAGICK='graphicsmagick';
+    private $avalibleEngine = array (self::ENGINE_GD2,self::ENGINE_IMAGEMAGICK,self::ENGINE_GRAPHIKSMAGICK);
+
+    /**
+     * initialization configuration
+     * @var array
+     */
+    private $_defaultConfig = array (
+        'engine'=>'gd2',
+        'cache'=>true,
+        'cacheBrowser'=>true,
+        'cacheDir'=>'/tmp/resizerCache/',
+        'tmpDir'=>'/tmp/'
+    );
+    private $_config;
+    private $_returnOnlyPath = false;
+    private $_checkETag;
+
+
+    public function __construct(array $cnf=array()) {
+        $this->_config=array_merge($this->_defaultConfig,$cnf);
+
+        if (!is_dir($this->_config['tmpDir']) OR !is_writable($this->_config['tmpDir'])) {
+            throw new PhpResizer_PhpResizerException('path '.$this->_config['tmpDir'].' is not exist or nor writible');
+        }
+
+        if ($this->_config['cache']
+            AND (!is_dir($this->_config['cacheDir'])
+            OR !is_writable($this->_config['cacheDir'])
+            OR !is_executable($this->_config['cacheDir']))) {
+            throw new PhpResizer_PhpResizerException('path '.$this->_config['cacheDir'].' is not exist or nor writible or no executable');
+        }
+
+        require_once ('engine/'.$this->_config['engine'].'.php');
+
+        $this->enginer = new $this->_config['engine']();
+    }
+
+
+    /**
+     *
+     * @param string $path
+     * @param array $options
+     */
+    public function resize($path, array $options = array()) {
+        if (!file_exists($path)) {
+            $this->_return404($path);
+        }elseif (!$size = @getimagesize($path)) {
+            throw new PhpResizer_PhpResizerException('file '.$path.' is crashed');
+        }
+
+        if (empty($options)) {
+            $this->_returnImageOrPath($path);
+        }
+
+        if (isset($options['returnOnlyPath']) AND $options['returnOnlyPath']=== true AND $this->_config['cache']) {
+                unset ($options['returnOnlyPath']);
+                $this->_returnOnlyPath = true;
+        }elseif(isset($options['returnOnlyPath']) AND $options['returnOnlyPath']=== true){
+                throw new PhpResizer_PhpResizerException('for returnOnlyPath turn cache TRUE');
+        }
+
+        $cacheFile = $this->_getCacheFileName($path,$options);
+
+
+        if ($this->enginer->resize(
+            $options+=array(
+            'path'=>$path,
+            'cacheFile'=>$cacheFile,
+            'size'=>$size))!==true) {
+            $this->_return404();
+        }
+
+
+        if (!$this->_config['cache']){
+            $this->_returnImageOrPath($cacheFile,$options);
+            unlink($cacheFile);
+            return;
+        }else{
+            return $this->_returnImageOrPath($cacheFile,$options);
+        }
+    }
+
+
+    /**
+     *
+     * return image if cacheFile is valid and exist OR return path to newcacheFile
+     * @param $options
+     * @param $path
+     */
+    private function _getCacheFileName ($path,$options) {
+        $cacheFile=null;
+
+        if ($this->_config['cache']) {
+            $cacheFile= $this->generatePath($path,$options);
+            if (file_exists($cacheFile) &&
+                getimagesize($cacheFile) &&
+                filemtime($cacheFile)>=filemtime($path)) {
+                    return $this->_returnImageOrPath($cacheFile,$options);
+            }elseif (file_exists($cacheFile)){
+                unlink($cacheFile);
+            }
+        }else{
+            $cacheFile=$this->_config['tmpDir'].'/imageResizerTmpFile_'.md5(microtime().mt_rand(100000,999999)).$this->getExtensionFilter($path);
+        }
+        return $cacheFile;
+    }
+
+    private function getExtensionFilter ($path) {
+        $fileInfoExtension = strtolower(pathinfo($path,PATHINFO_EXTENSION));
+        return (in_array($fileInfoExtension,array('png')))?'.'.$fileInfoExtension:'.jpg';
+    }
+
+    public function generatePath ($path,array $options){
+
+        if (isset($options['returnOnlyPath'])) {
+            unset ($options['returnOnlyPath']);
+        }
+//    	$cacheFilePath = $this->_config['cacheDir'].implode('_',$options).'/'.str_replace('/','-',$path).$this->getExtensionFilter($path);
+//    	if(!is_dir(dirname($cacheFilePath))){
+//            mkdir(dirname($cacheFilePath));
+//        }
+//    	return $cacheFilePath;
+//
+//    	$cacheFilePath = $this->_config['cacheDir'].'/'.str_replace('/','-',$path).$this->getExtensionFilter($path);
+//    	return $cacheFilePath;
+
+        $hash = md5(serialize($options).$path);
+        $cacheFilePath = $this->_config['cacheDir'].'/'.substr($hash, 0,2).'/'.substr($hash,2,2).'/'.substr($hash,4).$this->getExtensionFilter($path);
+        if(!is_dir(dirname(dirname($cacheFilePath)))){
+            mkdir(dirname(dirname($cacheFilePath)));
+        }
+        if(!is_dir(dirname($cacheFilePath))){
+            mkdir(dirname($cacheFilePath));
+        }
+        return $cacheFilePath;
+    }
+
+    /**
+     * @param string $filename absolute path to image-file
+     */
+    private function _returnImageOrPath ($filename,array $options=array()) {
+
+        if ($this->_returnOnlyPath) {
+                return $filename;
+        }
+
+        if ($this->_checkETag($filename)) {
+            header("HTTP/1.1 304 Not Modified");
+        }else{
+
+            header("Content-type: image/jpeg");
+            header("Content-Length: ".@filesize($filename));
+            header('ETag: '.md5_file($filename));
+            readfile ($filename); exit();
+        }
+    }
+
+
+    private function _return404 ($fileName='') {
+        header('HTTP/1.1 404 Not Found');
+        echo 'file '.$fileName.' not found';
+          exit;
+    }
+
+    /**
+     *
+     * @param string $filename absolute path to image-file
+     * @return boolean
+     */
+    private function _checkETag ($filename) {
+        if (!$this->_config['cacheBrowser']) {
+            return false;
+        }
+        if (isset ($this->checkETag)) {
+            return $this->checkETag;
+        }
+        if (isset($_SERVER['HTTP_IF_NONE_MATCH'])&& md5_file($filename)==$_SERVER['HTTP_IF_NONE_MATCH']) {
+            $this->checkETag=true;
+            return true;
+        }else{
+            $this->checkETag=false;
+            return false;
+        }
+    }
+    /**
+     *
+     * @param integer $timeMinuts
+     */
+    public function clearCache ($timeMinuts=10080) {
+        $timeMinuts=(int)$timeMinuts;
+        $command = "find ".$this->_config['cacheDir']." \! -type d -amin +".$timeMinuts." -exec  rm -v '{}' ';'";
+        ob_start();
+            passthru($command);
+        $result = ob_get_clean();
+        return $result;
+    }
+}
--- sources/PhpResizer/engine/imagemagic.php
+++ sources/PhpResizer/engine/imagemagic.php
@@ -12,55 +12,55 @@
 require_once ('abstract.php');
 
 class imagemagic extends ImageEngine  {
-	
-	protected $types=array(1 => "gif", "png","jpg","bmp","tif");
-	
-	// linux command to ImageMagick convert
-	private $convertPath='convert';
-	
-	protected function _checkEngine () {
-		$command = $this->convertPath.' -version';
-		ob_start();
-			passthru($command);
-		$stringOutput = ob_get_clean();
-		
-		$resultSearch = strpos($stringOutput,'ImageMagick');
-		
-		if ($resultSearch===false) {
-			throw new ResizerException('Engine '.__CLASS__.' is not avalible');
-		}
-	}
-	
-	public function resize  (array $params=array()) {
-		$this->getParams($params);
-		$size = $this->params['size'];
-		$path = $this->params['path'];
-		$cacheFile = $this->params['cacheFile'];
-		extract($this->calculateParams());
- 			$command = $this->convertPath
- 				.' '.$this->addSlashe($path).' -crop'
- 				.' '.$srcWidth.'x'.$srcHeight.'+'.$srcX.'+'.$srcY
- 				.' -resize '.$dstWidth.'x'.$dstHeight
- 				.' -sharpen 1x10'
- 				//.' -colorspace GRAY'
-				//.' -posterize 32'
-				//.' -depth 8'
-				//.' -contrast'
-				//.' -equalize'
-				//.' -normalize'
-				//.' -gamma 1.2'
- 				.' -quality 75'
- 				//.' -blur 2x4'
- 				//.' -unsharp 0.2x0+300+0' 
-				//.' -font arial.ttf -fill white -box "#000000100" -pointsize 12 -annotate +0+10 "  '.$path.' "'
-				//.' -charcoal 2'
-				//.' -colorize 180'
-				//.' -implode 4'
-				//.' -solarize 10' ???
-				//.' -spread 5'
- 				.' '.$this->addSlashe($cacheFile);
 
-			exec($command);
-			return true;
-	}
+    protected $types=array(1 => "gif", "png","jpg","bmp","tif");
+
+    // linux command to ImageMagick convert
+    private $convertPath='convert';
+
+    protected function _checkEngine () {
+        $command = $this->convertPath.' -version';
+        ob_start();
+            passthru($command);
+        $stringOutput = ob_get_clean();
+
+        $resultSearch = strpos($stringOutput,'ImageMagick');
+
+        if ($resultSearch===false) {
+            throw new PhpResizer_PhpResizerException('Engine '.__CLASS__.' is not avalible');
+        }
+    }
+
+    public function resize  (array $params=array()) {
+        $this->getParams($params);
+        $size = $this->params['size'];
+        $path = $this->params['path'];
+        $cacheFile = $this->params['cacheFile'];
+        extract($this->calculateParams());
+             $command = $this->convertPath
+                 .' '.$this->addSlashe($path).' -crop'
+                 .' '.$srcWidth.'x'.$srcHeight.'+'.$srcX.'+'.$srcY
+                 .' -resize '.$dstWidth.'x'.$dstHeight
+                 .' -sharpen 1x10'
+                 //.' -colorspace GRAY'
+                //.' -posterize 32'
+                //.' -depth 8'
+                //.' -contrast'
+                //.' -equalize'
+                //.' -normalize'
+                //.' -gamma 1.2'
+                 .' -quality 75'
+                 //.' -blur 2x4'
+                 //.' -unsharp 0.2x0+300+0'
+                //.' -font arial.ttf -fill white -box "#000000100" -pointsize 12 -annotate +0+10 "  '.$path.' "'
+                //.' -charcoal 2'
+                //.' -colorize 180'
+                //.' -implode 4'
+                //.' -solarize 10' ???
+                //.' -spread 5'
+                 .' '.$this->addSlashe($cacheFile);
+
+            exec($command);
+            return true;
+    }
 }

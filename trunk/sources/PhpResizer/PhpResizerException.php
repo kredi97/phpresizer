55d39583a94269496d1285688cf06343357e0b10
andrew.stephanoff@gmail.com <andrew.stephanoff@gmail.com@1f887b24-dee8-016c-a32f-6742569e22d2>
2010-11-13 23:46:26 +0000

    extract exception to separate file

---
+++ trunk/sources/PhpResizer/PhpResizerException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @version $Revision$
+ * @category PhpResizer
+ * @package PhpResizer
+ * @author $Author$ $Date$
+ * @license New BSD license
+ * @copyright http://phpresizer.org/
+ */
+
+/**
+ * Standard exception for PhpResizer package.
+ */
+class PhpResizer_PhpResizerException extends Exception
+{
+}
--- trunk/sources/PhpResizer/engine/graphicsmagick.php
+++ trunk/sources/PhpResizer/engine/graphicsmagick.php
@@ -12,42 +12,42 @@
 require_once ('abstract.php');
 
 class graphicsmagick extends ImageEngine  {
-	
-	protected $types=array(1 => "gif", "png","jpg","bmp","tif");
-	
-	// linux command to GraphicksMagick
-	private $gmPath='gm';
-	
-	protected function _checkEngine () {
-		$command = $this->gmPath.' version';
-		ob_start();
-			passthru($command);
-		$stringOutput = ob_get_clean();
-		
-		$resultSearch = strpos($stringOutput,'GraphicsMagick');
-		
-		if ($resultSearch===false) {
-			throw new ResizerException('Engine '.__CLASS__.' is not avalible');
-		}
-	}
-	
-	public function resize  (array $params=array()) {
-		$this->getParams($params);
-		
-		$size = $this->params['size'];
-		$path = $this->params['path'];
-		$cacheFile = $this->params['cacheFile'];
 
-		extract($this->calculateParams());
- 			$command = $this->gmPath.' convert'
- 				.' '.$this->addSlashe($path).' -crop'
- 				.' '.$srcWidth.'x'.$srcHeight.'+'.$srcX.'+'.$srcY
- 				.' -resize '.$dstWidth.'x'.$dstHeight
- 				.' -sharpen 1x10'
- 				.' -quality 75'
- 				.' '.$this->addSlashe($cacheFile);
+    protected $types=array(1 => "gif", "png","jpg","bmp","tif");
 
-			exec($command);
-		return true;
-	}
+    // linux command to GraphicksMagick
+    private $gmPath='gm';
+
+    protected function _checkEngine () {
+        $command = $this->gmPath.' version';
+        ob_start();
+            passthru($command);
+        $stringOutput = ob_get_clean();
+
+        $resultSearch = strpos($stringOutput,'GraphicsMagick');
+
+        if ($resultSearch===false) {
+            throw new PhpResizer_PhpResizerException('Engine '.__CLASS__.' is not avalible');
+        }
+    }
+
+    public function resize  (array $params=array()) {
+        $this->getParams($params);
+
+        $size = $this->params['size'];
+        $path = $this->params['path'];
+        $cacheFile = $this->params['cacheFile'];
+
+        extract($this->calculateParams());
+             $command = $this->gmPath.' convert'
+                 .' '.$this->addSlashe($path).' -crop'
+                 .' '.$srcWidth.'x'.$srcHeight.'+'.$srcX.'+'.$srcY
+                 .' -resize '.$dstWidth.'x'.$dstHeight
+                 .' -sharpen 1x10'
+                 .' -quality 75'
+                 .' '.$this->addSlashe($cacheFile);
+
+            exec($command);
+        return true;
+    }
 }

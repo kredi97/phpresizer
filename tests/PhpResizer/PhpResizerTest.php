80e8863a0ad569a063c1a7348630226288bf2691
andrew.stephanoff <andrew.stephanoff@gmail.com>
2010-11-14 09:11:48 +0000

    merge files into one test case

--- tests/PhpResizer/PhpResizerTest.php
+++ tests/PhpResizer/PhpResizerTest.php
@@ -12,15 +12,15 @@
  * Родительский класс для все классов тетсирования Stickr
  */
 class Resizer_TestCase extends PHPUnit_Framework_TestCase {
-	static public $cacheDir;
-	static public $testFile;
-    
-	/**
-	 * Метод выполняется перед всеми тестами в текущем классе
-	 */
+    static public $cacheDir;
+    static public $testFile;
+
+    /**
+     * Метод выполняется перед всеми тестами в текущем классе
+     */
     public static function setUpBeforeClass(){
-    	self::$cacheDir=dirname(__FILE__).'/cache/';
-    	self::$testFile=dirname(__FILE__).'/ResizerTestFile/';
+        self::$cacheDir=dirname(__FILE__).'/cache/';
+        self::$testFile=dirname(__FILE__).'/ResizerTestFile/';
     }
 
     /**
@@ -33,8 +33,8 @@ class Resizer_TestCase extends PHPUnit_Framework_TestCase {
      * Метод выполняется перед каждым тестируемым методом
      */
     public function setUp (){
-    	$command = 'rm -rf '.self::$cacheDir.'*';
-    	exec ($command);
+        $command = 'rm -rf '.self::$cacheDir.'*';
+        exec ($command);
     }
 
     /**
@@ -42,17 +42,67 @@ class Resizer_TestCase extends PHPUnit_Framework_TestCase {
      */
     protected function tearDown(){
         $command = 'rm -rf '.self::$cacheDir.'*';
-    	//exec ($command);
+        //exec ($command);
     }
 
-    
+    /**
+     *
+     */
+    public function testResize1() {
+        $normalFiles = array ('normal.bmp','normal.jpg','normalCMYK.jpg','normal.gif');
+        foreach ($normalFiles as $file) {
+            foreach (array(Resizer::ENGINE_GRAPHIKSMAGICK,Resizer::ENGINE_IMAGEMAGICK,Resizer::ENGINE_GD2) as $engine) {
+
+                $resizer = new Resizer(array(
+                    'engine'=>$engine,
+                    'cacheDir'=>self::$cacheDir
+                    )
+                );
+
+                $opt = array(
+                    'width'=>100,
+                    'height'=>150,
+                    'crop'=>75,
+                    'aspect'=>false,
+                    'returnOnlyPath'=>true
+                );
+
+                $fileInfoExtension = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+                $fileInfoExtensionFilter = (in_array($fileInfoExtension,array('png')))?$fileInfoExtension:'jpg';
+
+                $cacheFile = $resizer->generatePath(self::$testFile.$file,$opt);
+
+                //echo $file.' -- '.$cacheFile."\n";
+                $this->assertEquals($fileInfoExtensionFilter,pathinfo($cacheFile,PATHINFO_EXTENSION),'расширение исходного и ужатого файла (движок:'.$engine.') (файл:'.$file.')');
+
+                try {
+                    $resizer->resize(self::$testFile.$file,$opt);
+                }catch (ResizerException $e) {
+                    if ($engine == Resizer::ENGINE_GD2 and $fileInfoExtension =='bmp') {
+                        continue;
+                    }
+                    $this->assertTrue(false, 'Не перехваченное исключение (движок:'.$engine.') (файл:'.$file.')');
+                }
+
+                $this->assertTrue(file_exists($cacheFile), 'Наличие файла в кэше (движок:'.$engine.') (файл:'.$file.')');
+
+                $getimagesize = getimagesize($cacheFile);
+
+                $this->assertEquals($getimagesize[0],$opt['width'],'ширина файла (движок:'.$engine.') (файл:'.$file.')');
+                $this->assertTrue($opt['height']-1<=$getimagesize[1] AND $getimagesize[1]<=$opt['height']+1 ,'высота файла (движок:'.$engine.') (файл:'.$file.')');
+            }
+        }
+    }
+
+
+
     /**
      *
      * @param string $url
      * @param array $options
      * @return array
      */
-    
+
     protected function sendGET ($url,array $options=array()) {
          $defaultOptions = array(
              'session' => false

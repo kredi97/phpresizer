d9baf9cdf5546fe4ba460014d4328740e3883f60
andrew.stephanoff@gmail.com <andrew.stephanoff@gmail.com@1f887b24-dee8-016c-a32f-6742569e22d2>
2010-11-14 09:08:16 +0000

    reorganize unit tests directory structure

---
+++ trunk/tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../'));
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/sources'),
+    get_include_path(),
+)));
+
+require 'PhpResizer/Autoloader.php';
+new PhpResizer_Autoloader();
